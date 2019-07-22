6759762f0567896b1b57d8f9f3706276a14af821
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-22 22:27:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ class Trip {
     return store.drivers.filter(function (driver){
       return driver.id === this.driverId
     }.bind(this))[0];
+  }
   passenger(){
       return store.passengers.filter(function (driver){
         return passenger.id === this.passengerId
