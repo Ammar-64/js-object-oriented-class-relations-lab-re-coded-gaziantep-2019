fe3706ef1fbc682f6db40c75c6ab8cb4fb0c0aa9
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-22 22:13:33 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,47 @@
+let store = { drivers:[], trips:[], passengers:[]}
+
+let driverId = 0
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this);
+  }
+  trips(){
+    return store.trips.filte(trip => trip.driverId === this.id)
+  }
+  passengers(){
+    return this.trip().map(trip => trip.passenger)
+  }
+}
+
+let passengerId = 0
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId;
+    this.name = name;
+
+    store.passengers.push(this);
+  }
+  driver() {
+        return store.drivers.filter(
+            function(item) {
+                return item.driverId === this.id;
+            }.bind(this)
+        );
+    }
+
+}
+
+let tripId = 0
+
+class Trip {
+  constructor(name) {
+    this.id = ++tripId;
+    this.name = name;
+    store.trips.push(this);
+  }
+
+}
